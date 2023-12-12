f4a3d2498f13f6703c04f4576395710be8bd2dae
faissal17 <faissalaoukacha@gmail.com>
2023-12-12 11:54:06 +0100

    get apartement function is done

--- backend/src/controller/apartment/apartment.controller.js
+++ backend/src/controller/apartment/apartment.controller.js
@@ -1,7 +1,7 @@
 const Apartement = require("../../model/apartment.schema");
 
 class ApartementController {
-  static create = async (req, res) => {
+  static createApartement = async (req, res) => {
     const { name, description, price, city, adress, room, floor } = req.body;
     try {
       const appartement = await Apartement.create({
@@ -14,6 +14,20 @@ class ApartementController {
         floor,
       });
       res.status(201).json({ message: "appartement created", appartement });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+  static getApartement = async (req, res) => {
+    console.log("waaaaaaayeh");
+    try {
+      const apartement = await Apartement.find();
+      if (!apartement) {
+        return res.status(404).json({ message: "No apartments found" });
+      } else {
+        return res.status(200).json({ message: apartement });
+      }
     } catch (error) {
       console.error(error);
       res.status(500).json({ message: "Internal server error" });
--- backend/src/routes/apartement/apartement.routes.js
+++ backend/src/routes/apartement/apartement.routes.js
@@ -3,6 +3,8 @@ const apartementRoutes = express.Router();
 
 const ApartementController = require("../../controller/apartment/apartment.controller");
 
-apartementRoutes.post("/create", ApartementController.create);
+apartementRoutes.post("/create", ApartementController.createApartement);
+apartementRoutes.get("/get", ApartementController.getApartement);
+
 
 module.exports = apartementRoutes;
