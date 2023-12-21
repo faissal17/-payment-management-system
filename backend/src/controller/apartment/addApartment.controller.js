4a96add1bfdc2f1d456f4427ae652aed19ba0c6c
faissal17 <faissalaoukacha@gmail.com>
2023-12-21 19:12:50 +0100

    add apartement test

--- backend/src/controller/apartment/addApartment.controller.js
+++ backend/src/controller/apartment/addApartment.controller.js
@@ -3,6 +3,11 @@ const Apartement = require("../../model/apartment.schema");
 const createApartement = async (req, res) => {
   const { name, description, price, city, adress, room, floor, image } =
     req.body;
+  if (!name || !description || !price || !city || !adress || !room || !floor) {
+    return res
+      .status(400)
+      .json({ status: "error", message: "All fields are required" });
+  }
   try {
     const appartement = await Apartement.create({
       name,
@@ -14,7 +19,9 @@ const createApartement = async (req, res) => {
       room,
       floor,
     });
-    res.status(201).json({ message: "appartement created", appartement });
+    res
+      .status(201)
+      .json({ status: "succes", message: "Apartment created", appartement });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Internal server error" });
